c6d340e33a3f80abc6079211729b429b3aa0f1de
Paulhakeem <paulnyamawi18@gmail.com>
2023-09-26 21:08:50 +0300

    make it responsive

--- src/views/createAccount.vue
+++ src/views/createAccount.vue
@@ -73,9 +73,9 @@ const useFacebook = () => {
 <div class="min-h-auto bg-gray-100 py-6 flex flex-col justify-center sm:py-12">
 	<div class="relative py-3 sm:max-w-xl sm:mx-auto">
 		<div
-			class="absolute inset-0 bg-gradient-to-r from-green-200 to-primary shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl">
+			class="container1 absolute inset-0 bg-gradient-to-r from-green-200 to-primary shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl">
 		</div>
-		<div class="relative px-4 py-8 bg-white shadow-lg sm:rounded-3xl sm:p-20">
+		<div class="container1 relative px-4 py-8 bg-white shadow-lg sm:rounded-3xl sm:p-20">
 			<div class="max-w-md mx-auto">
 				<div>
 					<h1 class="text-2xl font-medium text-gray-500 tracking-wider">Create your account</h1>
