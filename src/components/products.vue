3dd19a8f61fe704da327377f3e33bb6f34e49dbe
Paulhakeem <paulnyamawi18@gmail.com>
2024-08-03 00:28:46 +0300

    sleeping mode

--- src/components/products.vue
+++ src/components/products.vue
@@ -1,43 +1,64 @@
 <script setup>
-import { usePriceStore } from '../stores/price.js'
+import { usePriceStore } from "../stores/price.js";
 
-const priceStore = usePriceStore()
-
+const priceStore = usePriceStore();
 </script>
 <template>
-<div>
-  <div
-    class="p-3 bg-primary text-center mx-4 my-10 rounded-tl-md rounded-tr-md"
-  > 
-    <p class="text-white font-light text-2xl text-center mr-4">Flash Sales</p>
-  </div>
+  <div>
+    <div
+      class="p-3 bg-primary text-center mx-4 my-10 rounded-tl-md rounded-tr-md"
+    >
+      <p class="text-white font-light text-2xl text-center mr-4">Flash Sales</p>
+    </div>
 
-  <div class="flex flex-wrap gap-6 mx-4 my-8 items-center justify-center">
-    <div v-for="product in priceStore.products" :key="product.id">
-      <div class="border-2 border-[#f4f2f3] rounded-md w-72 h-full">
-        <img :src="product.product_iamge_url" alt="" class="w-72" />
-        <h2 class="mx-3 text-md font-semibold font-sans pt-2 text-[#393333]">
-          {{ product.product_name }}
-        </h2>
-        <p class="mx-3 text-xs font-light text-[#9e9d9d] text-left">
-          {{ product.product_descrption }} 
-        </p>
-        <span class="mx-3 text-md font-semibold font-sans pt-2 text-[#393333]"
-          >${{ product.product_price }}</span
-        >
-        <div class="mb-3">
-          <button
-          @click="priceStore.addItems(product.id)"
+    <div class="flex flex-wrap gap-6 mx-4 my-8 items-center justify-center">
+      <div v-for="product in priceStore.products" :key="product.id">
+        <div class="border-2 border-[#f4f2f3] rounded-md w-72 h-full">
+          <img :src="product.product_iamge_url" alt="" class="w-72" />
+          <h2 class="mx-3 text-md font-semibold font-sans pt-2 text-[#393333]">
+            {{ product.product_name }}
+          </h2>
+          <p class="mx-3 text-xs font-light text-[#9e9d9d] text-left">
+            {{ product.product_descrption }}
+          </p>
+          <span class="mx-3 text-md font-semibold font-sans pt-2 text-[#393333]"
+            >${{ product.product_price }}</span
+          >
+          <!-- button -->
+          <div @click="priceStore.addItems(product.id)" class="mx-2 mb-3">
+            <div
+              class="inline-flex overflow-hidden text-white bg-primary rounded group"
+            >
+              <span
+                class="px-3.5 py-2 text-white bg-black group-hover:bg-gray-800 flex items-center justify-center"
+              >
+                <svg
+                  class="w-5 h-5"
+                  fill="none"
+                  stroke="currentColor"
+                  viewBox="0 0 24 24"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    stroke-linecap="round"
+                    stroke-linejoin="round"
+                    stroke-width="2"
+                    d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z"
+                  ></path>
+                </svg>
+              </span>
+              <span class="pl-4 pr-5 py-2.5">ADD CART</span>
+            </div>
+            <!-- <button
+          
             class="mx-3 text-md font-light mt-3 items-left bg-primary
              text-white p-1 rounded-full w-28 hover:text-primary hover:border-2 hover:border-primary hover:bg-white"
           >
             ADD CART
-          </button>
-         
+          </button> -->
+          </div>
         </div>
       </div>
     </div>
   </div>
-</div>
-  
 </template>
